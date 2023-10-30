089564481c072a01111aed53b39df788f517f137
shivam singh <ssultanpura9@gmail.com>
2023-10-29 19:43:55 -0700

    some css things did in login and signup page

---
+++ src/login/login.js
@@ -0,0 +1,72 @@
+import axios from "axios";
+import { useRef } from "react";
+import logincss from "../login/login.module.css";
+import { SiMinutemailer } from "react-icons/si";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { NavLink } from "react-router-dom";
+const Login = () => {
+  const emailref = useRef();
+  const passwordref = useRef();
+  const logindetailHandler = () => {
+    const myobj = {
+      email: emailref.current.value,
+      password: passwordref.current.value,
+    };
+    axios
+      .post("http://localhost:3000/user/login", myobj)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className={logincss.loginmain}>
+      {" "}
+      <div className={logincss.container}>
+        <div className={logincss.leftContainer}>
+          <div className={logincss.logo}></div>
+          <div className={logincss.welcome}>
+            <h1>Welcome Page</h1>
+            <h4>login now & enjoy all features </h4>
+            <p>chitchat messenger</p>
+          </div>
+        </div>
+        <div className={logincss.rightContainer}>
+          <div className={logincss.frame}>
+            <div className={logincss.signInTitle}>LOG-IN</div>
+            <div className={logincss.form}>
+              <div className={logincss.emailInput}>
+                <SiMinutemailer className={logincss.icon}></SiMinutemailer>
+                <input ref={emailref} placeholder="Enter email id"></input>
+              </div>
+              <div className={logincss.passwordInput}>
+                <RiLockPasswordLine
+                  className={logincss.icon}
+                ></RiLockPasswordLine>
+                <input ref={passwordref} placeholder="Enter password"></input>
+              </div>
+            </div>
+            <div className={logincss.forgetPassword}>
+              <NavLink to="/forget">forget password</NavLink>
+            </div>
+            <div className={logincss.button}>
+              <button
+                className={logincss.loginbutton}
+                onClick={logindetailHandler}
+              >
+                Signin
+              </button>
+            </div>
+            <h3>or</h3>
+            <div className={logincss.button}>
+              <button className={logincss.signupbutton}>SignUp</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;
---
+++ src/signup/signup.js
@@ -0,0 +1,98 @@
+import { useRef } from "react";
+import axios from "axios";
+import signupcss from "./signup.module.css";
+import { SiMinutemailer } from "react-icons/si";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { NavLink } from "react-router-dom";
+const SignUp = () => {
+  const emailref = useRef();
+  const passwordref = useRef();
+  const nameref = useRef();
+  const mobileref = useRef();
+
+  const signupHandler = () => {
+    const emailValue = emailref.current.value;
+    const mobileValue = mobileref.current.value;
+    const nameValue = nameref.current.value;
+    const passworValue = passwordref.current.value;
+
+    const myObj = {
+      email: emailValue,
+      password: passworValue,
+      name: nameValue,
+      mobile: mobileValue,
+    };
+    console.log(myObj);
+    axios
+      .post("http://localhost:3000/user/savedata", myObj)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className={signupcss.loginmain}>
+      {" "}
+      <div className={signupcss.container}>
+        <div className={signupcss.leftContainer}>
+          <div className={signupcss.logoContainer}> <div className={signupcss.logo}></div></div>
+         
+          <div className={signupcss.welcome}>
+            <h1>Welcome User</h1>
+            <div className={signupcss.paragraph}>
+              <h3>register now on our platform & get all benefits :</h3>
+             <ul>
+              <li>sending and recieving messages</li>
+              <li>manage multiple groups as admin </li>
+              <li>and other premium features</li>
+             </ul>
+             
+            </div>
+            <p className={signupcss.companyname}>chitchat messenger</p>
+          </div>
+        </div>
+        <div className={signupcss.rightContainer}>
+          <div className={signupcss.frame}>
+            <div className={signupcss.signUpTitle}>SIGN-UP</div>
+            <div className={signupcss.form}>
+              <div className={signupcss.emailInput}>
+                <SiMinutemailer className={signupcss.icon}></SiMinutemailer>
+                <input ref={nameref} placeholder="Enter your name"></input>
+              </div>
+              <div className={signupcss.emailInput}>
+                <SiMinutemailer className={signupcss.icon}></SiMinutemailer>
+                <input ref={emailref} placeholder="Enter email id"></input>
+              </div>
+              <div className={signupcss.passwordInput}>
+                <RiLockPasswordLine
+                  className={signupcss.icon}
+                ></RiLockPasswordLine>
+                <input ref={passwordref} placeholder="Enter password"></input>
+              </div>
+            </div>
+            <div className={signupcss.passwordInput}>
+              <RiLockPasswordLine
+                className={signupcss.icon}
+              ></RiLockPasswordLine>
+              <input ref={mobileref} placeholder="Enter mobile no"></input>
+            </div>
+
+            <div className={signupcss.button}>
+              <button className={signupcss.loginbutton} onClick={signupHandler}>
+                SignUp
+              </button>
+            </div>
+            <h3>or</h3>
+            <div className={signupcss.button}>
+              <button className={signupcss.signupbutton}>SignIn</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
+};
+export default SignUp;
